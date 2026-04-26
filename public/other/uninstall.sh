1b8134d9b1ec920a101ccf5448fbac7700179d50
falrux <142692930+falrux@users.noreply.github.com>
2026-04-26 01:08:12 -0500

    uninstaller fr

---
+++ public/other/uninstall.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+noxdir="$HOME/Documents/Noxium"
+
+sudo -v
+
+sudo killall -9 Noxium 2>/dev/null
+sudo killall -9 noxium 2>/dev/null # i forgot
+sudo killall -9 Roblox 2>/dev/null
+sudo killall -9 RobloxPlayer 2>/dev/null
+
+if [ -d "/Applications/Noxium.app" ]; then
+    sudo rm -rf "/Applications/Noxium.app"
+fi
+
+if [ -d "$noxdir" ]; then
+    rm -rf "$noxdir"
+fi
+
+echo "uninstalled ok bai"
